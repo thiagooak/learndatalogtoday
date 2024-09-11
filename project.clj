739ebea25d0540d36c82b5b66c1513f9363772fd
Thiago Carvalho <thiago.oak@gmail.com>
2024-09-11 13:56:58 -0300

    Update Dependencies

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
 (defproject learndatalogtoday "0.1.0"
   :description "Interactive Datalog Tutorial"
   :url "https://datomic.learn-some.com"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/clojurescript "1.11.132"]
-                 [compojure "1.5.1"]
-                 [ring/ring-jetty-adapter "1.5.0"]
-                 [com.datomic/datomic-free "0.9.5394"]
+                 [compojure "1.7.1"]
+                 [ring/ring-jetty-adapter "1.12.2"]
+                 [com.datomic/peer "1.0.7187"]
                  [datomic-query-helpers "0.1.1"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "0.9.89"]
-                 [fipp "0.6.6"]
-                 [com.taoensso/timbre "4.7.4"]
+                 [markdown-clj "1.12.1"]
+                 [fipp "0.6.26"]
+                 [com.taoensso/timbre "4.10.0"]
                  [org.clojure/core.rrb-vector "0.1.2"]
                  ;; cljs
                  [hylla "0.2.0"]
                  [hiccups "0.3.0"]
                  [domina "1.0.3"]]
-  :plugins [[lein-ring "0.9.7"]
-            [lein-cljsbuild "1.1.4"]]
+  :plugins [[lein-ring "0.12.6"]
+            [lein-cljsbuild "1.1.8"]]
   :source-paths ["src/clj"]
   :ring {:handler learndatalogtoday.handler/app}
   :main learndatalogtoday.handler
